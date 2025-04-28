35ccfe4475b42e5a44951d7b70707ee11c4f0096
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-04-28 22:39:54 +0100

    Removed lazy_static in favour of LazyLock

--- src/timezone_manager.rs
+++ src/timezone_manager.rs
@@ -1,50 +1,49 @@
 use std::collections::HashMap;
+use std::sync::LazyLock;
 
 use async_trait::async_trait;
 use chrono_tz::{America, Asia, Europe, Tz};
-use lazy_static::lazy_static;
 use serenity::all::UserId;
-use sqlx::{any::AnyQueryResult, Database, Pool};
+use sqlx::{Database, Pool, any::AnyQueryResult};
 
-lazy_static! {
-    pub static ref LOCALE_TO_TIMEZONE: HashMap<&'static str, chrono_tz::Tz> = {
-        let mut m = HashMap::new();
-        m.insert("id", Asia::Jakarta);
-        m.insert("da", Europe::Copenhagen);
-        m.insert("de", Europe::Berlin);
-        m.insert("en-GB", Europe::London);
-        // m.insert("en-US", chrono_tz::America::New_York);
-        m.insert("es-ES", Europe::Madrid);
-        m.insert("es-419", America::Mexico_City);
-        m.insert("fr", Europe::Paris);
-        m.insert("hr", Europe::Zagreb);
-        m.insert("it", Europe::Rome);
-        m.insert("lt", Europe::Vilnius);
-        m.insert("hu", Europe::Budapest);
-        m.insert("nl", Europe::Amsterdam);
-        m.insert("no", Europe::Oslo);
-        m.insert("pl", Europe::Warsaw);
-        m.insert("pt-BR", America::Sao_Paulo);
-        m.insert("ro", Europe::Bucharest);
-        m.insert("fi", Europe::Helsinki);
-        m.insert("sv-SE", Europe::Stockholm);
-        m.insert("vi", Asia::Ho_Chi_Minh);
-        m.insert("tr", Europe::Istanbul);
-        m.insert("cs", Europe::Prague);
-        m.insert("el", Europe::Athens);
-        m.insert("bg", Europe::Sofia);
-        m.insert("ru", Europe::Moscow);
-        m.insert("uk", Europe::Kiev);
-        m.insert("hi", Asia::Kolkata);
-        m.insert("th", Asia::Bangkok);
-        m.insert("zh-CN", Asia::Shanghai);
-        m.insert("ja", Asia::Tokyo);
-        m.insert("zh-TW", Asia::Taipei);
-        m.insert("ko", Asia::Seoul);
-        m
-    };
-
-}
+pub static LOCALE_TO_TIMEZONE: LazyLock<HashMap<&'static str, chrono_tz::Tz>> =
+    LazyLock::new(|| {
+        [
+            ("id", Asia::Jakarta),
+            ("da", Europe::Copenhagen),
+            ("de", Europe::Berlin),
+            ("en-GB", Europe::London),
+            ("es-ES", Europe::Madrid),
+            ("es-419", America::Mexico_City),
+            ("fr", Europe::Paris),
+            ("hr", Europe::Zagreb),
+            ("it", Europe::Rome),
+            ("lt", Europe::Vilnius),
+            ("hu", Europe::Budapest),
+            ("nl", Europe::Amsterdam),
+            ("no", Europe::Oslo),
+            ("pl", Europe::Warsaw),
+            ("pt-BR", America::Sao_Paulo),
+            ("ro", Europe::Bucharest),
+            ("fi", Europe::Helsinki),
+            ("sv-SE", Europe::Stockholm),
+            ("vi", Asia::Ho_Chi_Minh),
+            ("tr", Europe::Istanbul),
+            ("cs", Europe::Prague),
+            ("el", Europe::Athens),
+            ("bg", Europe::Sofia),
+            ("ru", Europe::Moscow),
+            ("uk", Europe::Kiev),
+            ("hi", Asia::Kolkata),
+            ("th", Asia::Bangkok),
+            ("zh-CN", Asia::Shanghai),
+            ("ja", Asia::Tokyo),
+            ("zh-TW", Asia::Taipei),
+            ("ko", Asia::Seoul),
+        ]
+        .into_iter()
+        .collect()
+    });
 
 #[async_trait]
 pub trait TimezoneManager<Db: Database> {
